b4c0385731147fc39cd5c0addf9b2110a86cdcca
bikalthapa <bikalt40@gmail.com>
2025-01-02 08:12:06 +0545

    Character movement

--- script/script.js
+++ script/script.js
@@ -11,9 +11,9 @@ ctx.scale(dpr, dpr);
 
 
 // Game data
-let rowlen = 10;
-let collen = 10;
-let cellWidth = 20;
+let rowlen = 25;
+let collen = 25;
+let cellWidth = 15;
 let startPos = { x: 7, y: 7 };
 let lineColor = "blue";
 let lineWidth = 2;
@@ -26,6 +26,7 @@ class Cell {
         this.width = dimension.width; // Cell width
         this.height = dimension.height; // Cell height
         this.isVisited = false;
+        this.walls = [true,true,true,true];//Initially top, right, buttom and left are set with walls
         this.neighbour = { pos: [], selectOrder: [], count: 0 };
         this.pos = { x: cellPos.x, y: cellPos.y }; // Position on canvas
         this.index = { x: Math.floor(cellPos.x / dimension.width), y: Math.floor(cellPos.y / dimension.height) };
@@ -144,7 +145,49 @@ class Cell {
     getNextNeighbourIndx() {
         return (this.neighbour.pos[this.neighbour.selectOrder[this.neighbour.count]]);
     }
-
+    setWalls(border,value){
+        if(border=="top"){
+            this.walls[0] = value;
+        }else if(border=="right"){
+            this.walls[1] = value;
+        }else if(border=="bottom"){
+            this.walls[2] = value;
+        }else{
+            this.walls[3] = value;
+        }
+    }
+    findAllWalls(totalRow, totalCol) {
+        let neighbourIndex = [];
+    
+        
+    
+        // Check the top neighbor
+        let tempPos = { x: this.index.x, y: this.index.y - 1 };
+        if (tempPos.y >= 0 && !board[tempPos.x][tempPos.y].isVisited) {
+            this.walls[0]
+        }
+    
+        // Check the right neighbor
+        tempPos = { x: this.index.x + 1, y: this.index.y };
+        if (tempPos.x < totalCol && !board[tempPos.x][tempPos.y].isVisited) {
+            this.neighbour.pos[1] = { x: tempPos.x, y: tempPos.y };
+            neighbourIndex.push(1);
+        }
+    
+        // Check the bottom neighbor
+        tempPos = { x: this.index.x, y: this.index.y + 1 };
+        if (tempPos.y < totalRow && !board[tempPos.x][tempPos.y].isVisited) {
+            this.neighbour.pos[2] = { x: tempPos.x, y: tempPos.y };
+            neighbourIndex.push(2);
+        }
+    
+        // Check the left neighbor
+        tempPos = { x: this.index.x - 1, y: this.index.y };
+        if (tempPos.x >= 0 && !board[tempPos.x][tempPos.y].isVisited) {
+            this.neighbour.pos[3] = { x: tempPos.x, y: tempPos.y };
+            neighbourIndex.push(3);
+        }
+    }
 }
 
 
@@ -177,14 +220,22 @@ function removeLineBetwen(currCell, nextCell) {
     if (currCell.index.x == nextCell.index.x) {// Next cell is at either top or bottom side of current cell
         if (currCell.index.y > nextCell.index.y) {//Next cell lies at top
             currCell.eraseLine(ctx, 'top');
-        } else {
+            currCell.setWalls("top",false);
+            nextCell.setWalls("bottom",false);
+        } else {// Next cell lies at bottom
             currCell.eraseLine(ctx, 'bottom');
+            currCell.setWalls("bottom",false);
+            nextCell.setWalls("top",false);
         }
     } else if (currCell.index.y == nextCell.index.y) {// Next cell is at either left or right side of current cell 
         if (currCell.index.x > nextCell.index.x) {
             currCell.eraseLine(ctx, 'left');
+            currCell.setWalls("left",false);
+            nextCell.setWalls("right",false);
         } else {
             currCell.eraseLine(ctx, 'right');
+            currCell.setWalls("right",false);
+            nextCell.setWalls("left",false);
         }
     }
 }
@@ -255,20 +306,109 @@ function generateMazeDFS(startIndx) {
     indicateDestination(destinationCell, 'red'); // Indicate the destination cell with a red color
 }
 
-function generateMazePRIMS(startIndx){
 
+/*
+This algorithm is a randomized version of Prim's algorithm.
+
+1. Start with a grid full of walls.
+2. Pick a cell, mark it as part of the maze. Add the walls of the cell to the wall list.
+3. While there are walls in the list:
+    1.Pick a random wall from the list. If only one of the cells that the wall divides is visited, then:
+        1. Make the wall a passage and mark the unvisited cell as part of the maze.
+        2. Add the neighboring walls of the cell to the wall list.
+    2. Remove the wall from the list.
+*/
+function generateMazePRIMS(startIndx){
+    let startCell = board[startIndx.x][startIndx.y];
+    
 }
 // Function to indicate a specific cell (destination) on the canvas
 function indicateDestination(cell, color) {
     ctx.fillStyle = color;
     ctx.fillRect(cell.pos.x + cell.lineWidth / 2, cell.pos.y + cell.lineWidth / 2, cell.width - cell.lineWidth, cell.height - cell.lineWidth);
-    console.log("Destination Cell:", cell.pos); // Log the destination cell's position
 }
+
+
+
+
+
+function getWallIndx(fromIndx, toIndx){
+    if (fromIndx.x == toIndx.x) {
+        if (fromIndx.y > toIndx.y) {
+            return 0; // Top wall
+        } else {
+            return 2; // Bottom wall
+        }
+    } else if (fromIndx.y == toIndx.y) {
+        if (fromIndx.x > toIndx.x) {
+            return 3; // Left wall
+        } else {
+            return 1; // Right wall
+        }
+    }
+    return false;
+}
+
+function moveCharacter(fromIndx, toIndx){
+    let currentCell = board[fromIndx.x][fromIndx.y];
+    let nextCell = board[toIndx.x][toIndx.y];
+    currentCell.indicateCell(ctx, "white");
+    nextCell.indicateCell(ctx, "red");
+}
+
+function isValidMoves(fromIndx, toIndx){
+    if (toIndx.x >= 0 && toIndx.x < board.length && toIndx.y >= 0 && toIndx.y < board[0].length) {
+        let wallIndx = getWallIndx(fromIndx, toIndx);
+            let currentCell = board[fromIndx.x][fromIndx.y];
+            if (currentCell.walls[wallIndx]==false) {
+                return true;
+            }
+    }
+    return false;
+}
+
+let characterIndx = {x: 0, y: 0};
+
+document.addEventListener('keydown', function(event) {
+    let tempPos;
+    switch (event.key) {
+        case 'ArrowDown':
+            tempPos = {x: characterIndx.x, y: characterIndx.y + 1};
+            if (isValidMoves(characterIndx, tempPos)) {
+                moveCharacter(characterIndx, tempPos);
+                characterIndx.y++;
+            }
+            break;
+        case 'ArrowLeft':
+            tempPos = {x: characterIndx.x - 1, y: characterIndx.y};
+            if (isValidMoves(characterIndx, tempPos)) {
+                moveCharacter(characterIndx, tempPos);
+                characterIndx.x--;
+            }
+            break;
+        case 'ArrowUp':
+            tempPos = {x: characterIndx.x, y: characterIndx.y - 1};
+            if (isValidMoves(characterIndx, tempPos)) {
+                moveCharacter(characterIndx, tempPos);
+                characterIndx.y--;
+            }
+            break;
+        case 'ArrowRight':
+            tempPos = {x: characterIndx.x + 1, y: characterIndx.y};
+            if (isValidMoves(characterIndx, tempPos)) {
+                moveCharacter(characterIndx, tempPos);
+                characterIndx.x++;
+            }
+            break;
+    }
+});
+
 
 
 
 createGrid(startPos, cellWidth, rowlen, collen, lineColor, lineWidth);
 board[0][0].indicateCell(ctx, "red");
+
 // board[0][1].isVisited = true;
 // board[0][1].indicateCell(ctx, "green");
 // board[1][0].isVisited = true;
